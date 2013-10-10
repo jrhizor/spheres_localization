0a013070b91f0a4dd5cb2d14bd9374e86ca4b47e
acmcarther <acmcarther@gmail.com>
2013-10-09 17:30:33 -0700

    Rewrote registration

--- src/registration/registration.cpp
+++ src/registration/registration.cpp
@@ -8,26 +8,78 @@
 
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include <sstream>
 #include <pcl/io/pcd_io.h>
 #include <pcl/point_types.h>
 #include <pcl/filters/passthrough.h>
 #include <pcl/registration/icp.h>
-#include <pcl/visualization/cloud_viewer.h>
+#include <pcl/visualization/pcl_visualizer.h>
+
+//convenient typedefs
+typedef pcl::PointXYZ PointT;
+typedef pcl::PointCloud<PointT> PointCloud;
+
+bool loadPointClouds(char* filepath, std::vector<PointCloud::Ptr> cloudSet)
+{
+	// Define function variables
+	std::ifstream fin;
+	std::string directory = std::string(filepath);
+	std::string fname = directory + "info.txt";
+	int filecount;
+
+
+	// Open the information file
+	fin.open(fname.c_str());
+	if(!fin.good())
+	{
+		return false;
+	}
+	fin >> filecount;
+	fin.close();
+
+	// Load all of the pointclouds
+	for(int index=0; index < filecount; index++)
+	{
+		PointCloud::Ptr tempCloud (new PointCloud);
+
+		// Get the pointcloud's name
+		std::stringstream ss;
+		ss << directory << "/depth/" << index+1 << ".pcd";
+
+		// Read pointcloud
+		pcl::io::loadPCDFile(ss.str(), *tempCloud);
+
+		// Remove NAN values from pointcloud
+		std::vector<int> indices;
+		pcl::removeNaNFromPointCloud(*tempCloud,*tempCloud, indices);
+
+		// Add that pointcloud to our list
+		cloudSet.push_back(tempCloud);
+	}
+
+	return true;
+
+}
 
 int main(int argc, char* argv[]) 
 {
 	// Define function variables
 	std::ifstream fin;
 	std::ofstream fout;
-	int imageCount = -1;
-	pcl::PointCloud<pcl::PointXYZ>::Ptr totalCloud (new pcl::PointCloud<pcl::PointXYZ>);
-	pcl::PointCloud<pcl::PointXYZ>::Ptr newCloud (new pcl::PointCloud<pcl::PointXYZ>);
-	pcl::PointCloud<pcl::PointXYZ> resultCloud;
-	pcl::IterativeClosestPoint<pcl::PointXYZ, pcl::PointXYZ> icp;
-	int folderIndex = 0;
+	int imageIndex = -1, totalImages;
+	PointCloud::Ptr totalCloud (new PointCloud);
+	PointCloud::Ptr newCloud (new PointCloud);
+	PointCloud resultCloud;
+	pcl::IterativeClosestPoint<PointT, PointT> icp;
+	pcl::PassThrough<PointT> pass;
+	std::vector<PointCloud::Ptr> cloudSet;
+	std::string folderPath;
+
+	std::stringstream ss;
+
 	bool fileCorrelated;
-	char pcdFileName[] = "capture.pcd";
-	char folderPath[700];
+	char pcdFileName[] = "capture";
 	char totalPath[700];
 	char errorString[700];
 	char resultPath[700];
@@ -36,145 +88,49 @@ int main(int argc, char* argv[])
 	if(argc < 2)
 	{
 		// Tell the user to give us a file next time
-		std::cout << "Please specify filename." << std::endl;
+		std::cout << "Please specify filepath." << std::endl;
 
 		// Return failure
-		return 1;
+		return -1;
 	}
 
-	// Open file specified in command line
-	fin.open(argv[1]);
-
-	// Verify that file is open
-	if(!fin.good())
-	{
-		// Tell the user that he/she gave us a bad file
-		std::cout << "File not found." << std::endl;
-
-		// Return failure
-		return 1;
-	}
-
-	// Get the number of image/point cloud pairs
-	fin >> imageCount;
-
-	// Close file
-	fin.close();
+	// Read in the pointclouds from files
+	loadPointClouds(argv[1], cloudSet);
+	folderPath = argv[1];
 
 	// If count is less than 2, return failure
-	if(imageCount < 2)
+	if(cloudSet.size() < 2)
 	{
-		std::cout << "Operation invalid on less than two files." << std::endl;
+		PCL_ERROR ("Error reading files. \n");
 		return -1;
 	}
+	PCL_INFO("Loaded %d pointclouds.", (int)cloudSet.size());
 
-	// Read in first pointcloud into aggregate and save identity matrix there
-	if(pcl::io::loadPCDFile<pcl::PointXYZ> ("./0/capture.pcd", *totalCloud) == -1)
+	// TODO: Identity matrix for mat 1
+
+	// Copy pointcloud 1 into aggregated point cloud
+	*totalCloud = *cloudSet[1];
+
+	// Process all pointclouds
+	ss << folderPath << "transforms.txt";
+	fout.open(ss.str().c_str());
+	for(int index=1; index < cloudSet.size(); index++)
 	{
-		PCL_ERROR ("Could not load file ./0/capture.pcd \n");
-		return -1;
-	}
-	// TODO: Identity matrix
-
-	// Prime the loop
-	fileCorrelated = true;
-	imageCount--;
-	folderIndex++;
-
-	// While files remain, and no files are invalid, add pointclouds
-	while( imageCount > 0 && fileCorrelated == true)
-	{
-		std::cout << imageCount << " " << folderIndex << std::endl; 
-
-
-		// Disable correlation flag
-		fileCorrelated = false;
-
-		// Determine file path
-		sprintf(folderPath, "./%i",folderIndex);
-		sprintf(totalPath, "./%i/%s",folderIndex,pcdFileName);
-
-		std::cout << "Read in new pointcloud" << std::endl;
-		// Read in new pointcloud
-		if(pcl::io::loadPCDFile<pcl::PointXYZ> (totalPath, *newCloud) == -1)
-		{
-			sprintf(errorString, "Could not load file %s .\n", totalPath);
-			PCL_ERROR (errorString);
-			return -1;
-		}		
-
-
-		std::cout << "Align clouds." << std::endl;
-
-		std::cout << "size: " << newCloud->size() << " " << totalCloud->size() << std::endl;
-
-		// Run correlation from new cloud to 
-		pcl::PassThrough<pcl::PointXYZ> pass;
-		pass.setInputCloud(newCloud);
-		pass.filter(*newCloud);
-
-		pass.setInputCloud(totalCloud);
-		pass.filter(*totalCloud);
-
-		icp.setInputSource(newCloud);
-		std::cout << "afterinputset." << std::endl;
+		// Align the pointclouds		
+		icp.setInputSource(cloudSet[index]);
 		icp.setInputTarget(totalCloud);
-		std::cout << "aftertargetset." << std::endl;
 		icp.align(resultCloud);
-		std::cout << resultCloud.size() << std::endl;
 
-
-	pcl::visualization::CloudViewer viewer("Simple");
-	// viewer.showCloud(newCloud);
-	// while(!viewer.wasStopped()){}
-
-	// 		viewer.showCloud(totalCloud);
-	// while(!viewer.wasStopped()){}
-
-
-
-		std::cout << "Check for convergence." << std::endl;
-		// If correlation was successful, continue through
 		if( icp.hasConverged() )
 		{
+			// Add the transformed pointcloud to the aggregate
+			*totalCloud += resultCloud;
 
-			std::cout << "Creating transformation." << std::endl;
-			// Transform the input matrix
-
-			// Add the input matrix to the total matrix
-			*totalCloud = resultCloud;
-
-
-			viewer.showCloud(totalCloud);
-	while(!viewer.wasStopped()){}
-
-
-
-
-			// Save transform matrix to same directory as cloud
-			sprintf(resultPath, "%s/transform.txt", folderPath);
-			fout.open(resultPath);
-			fout << icp.getFinalTransformation();
-			fout.close();
-
-			// Let the loop continue
-			fileCorrelated = true;
-
+			// Output the transformation
+			fout << icp.getFinalTransformation() << std::endl;
 		}
-
-		imageCount--;
-		folderIndex++;
-		
 	}
-
-	std::cout << "Finished registration." << std::endl;
-
-
-
-
-
-
-
+	fout.close();
 
 	// Save the aggregated pointcloud in the root directory
 	pcl::io::savePCDFile ("totalCloud.pcd", *totalCloud);
