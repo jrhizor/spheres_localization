0f1560e907f596e095517ab543c3f1154df814f2
jrhizor <me@jaredrhizor.com>
2014-02-20 12:54:27 -0800

    Added distortion parameters.

--- include/spheres_localization/pose_estimation/pose_estimation.h
+++ include/spheres_localization/pose_estimation/pose_estimation.h
@@ -277,6 +277,11 @@ std::cout << good_matches.size() << std::endl;
   cv::Matx33f cameraMatrix(525.0, 0.0, 319.5, 0.0, 525.0, 239.5, 0.0, 0.0, 1.0);
 
   std::vector<float> distortions;
+  distortions.push_back(0.18126525); 
+  distortions.push_back(-0.39866885);
+  distortions.push_back(0);
+  distortions.push_back(0);
+  
   std::vector<int> inliers;
 
   //solvePnP(objectPoints, imagePoints, cameraMatrix, distortions, rvec, tvec, false, CV_EPNP);
